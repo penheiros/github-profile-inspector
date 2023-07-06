a29cb7947434f1b42a6d76777669a020c5fb471b
bennetcodes <83713963+penheiros@users.noreply.github.com>
2023-07-07 03:26:35 +0600

    Added more features
    More information on user.
    Link redirecting to user profile.
    Null value filtering.

--- script.js
+++ script.js
@@ -4,31 +4,39 @@ var form = document.getElementById("github-profile-form")
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var resultContainer = document.getElementById('result')
     var search = document.getElementById("search-username").value;
     var validatedName = search.split(' ').join('')
-    var resultContainer = document.getElementById('result')
+    resultContainer.style.height = 'auto';
 
     const userAvatar = document.getElementById('user-avatar');
     const userName = document.getElementById('user-name');
+    const userProfileLink = document.getElementById('user-profile-link');
     const userLoginName = document.getElementById('user-login-name');
     const userFollowers = document.getElementById('user-followers');
+    const userLocation = document.getElementById('user-location');
+    const userBlog = document.getElementById('user-blog');
     const userRepos = document.getElementById('user-public-repositories');
     const userBio = document.getElementById('user-bio');
 
     fetch(`https://api.github.com/users/${validatedName}`)
         .then(result => result.json())
         .then(data => {
+            for (key in data) {if(data[key] == null || data[key].length === 0) {data[key] = "Un-available"}}
             console.log(data);
             userAvatar.setAttribute("src", data.avatar_url);
             userName.innerHTML = data.name;
+            userProfileLink.innerHTML = `Profile Link`;
+            userProfileLink.setAttribute('href', data.html_url);
+            userProfileLink.setAttribute('target', target="_blank");
             userLoginName.innerHTML = `Username : ${data.login}`;
             userFollowers.innerHTML = `Followers : ${data.followers}`;
+            userLocation.innerHTML = `Location : ${data.location}`;
             userRepos.innerHTML = `Public Repositories : ${data.public_repos}`;
-            
-            if (data.bio == null) {
-                userBio.innerHTML = "Description : Empty";
-            } else {userBio.innerHTML = `Description : ${data.bio}`;}
+            userBio.innerHTML = `Description : ${data.bio}`;
+
+            if (data.blog.value === "Un-available") {userBlog.innerHTML = `Website : <a href=${data.blog} target="_blank">${data.blog}</a>`;
+            } else {userBlog.innerHTML = `Website : ${data.blog}`}
         })
     
-    resultContainer.style.height = 'auto';
 })
