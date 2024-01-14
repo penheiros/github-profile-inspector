0da9fa88194c75438ee46d221743db31d5e8bbd0
penheiros <penheiros@gmail.com>
2024-01-14 22:56:31 +0600

    User Interface Update

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ var form = document.getElementById("github-profile-form")
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var resultContainer = document.getElementById('result')
+    var resultBox = document.getElementById('result-box');
     var search = document.getElementById("search-username").value;
     var validatedName = search.split(' ').join('')
-    resultContainer.style.height = 'auto';
 
     const userAvatar = document.getElementById('user-avatar');
     const userName = document.getElementById('user-name');
@@ -19,21 +18,29 @@ form.addEventListener('submit', function(event) {
 
     fetch(`https://api.github.com/users/${validatedName}`)
         .then(result => result.json())
-        .then(data => {
-            for (key in data) {if(data[key] == null || data[key].length === 0) {data[key] = "Unavailable"}}
-            userAvatar.setAttribute("src", data.avatar_url);
-            userName.innerHTML = data.name;
-            userProfileLink.innerHTML = `Profile Link`;
-            userProfileLink.setAttribute('href', data.html_url);
-            userProfileLink.setAttribute('target', target="_blank");
-            userLoginName.innerHTML = `Username : ${data.login}`;
-            userFollowers.innerHTML = `Followers : ${data.followers}`;
-            userLocation.innerHTML = `Location : ${data.location}`;
-            userRepos.innerHTML = `Public Repositories : ${data.public_repos}`;
-            userBio.innerHTML = `Description : ${data.bio}`;
+        .then(data => {            
+            for (key in data) {
+                if(data[key] == null || data[key].length === 0){
+                    data[key] = "Unavailable"
+                }
+            };
+            
+            resultBox.innerHTML = `
+            <h1 class="result-title">Results</h1>
+            <div id="result">
+                <div class="userinfo">
+                    <img id="user-avatar", src=${data.avatar_url}>
+                    <p id="user-name">${data.name}</p>
+                    <a id="user-profile-link" href=${data.html_url} target='_blank'>Profile Link</a>
+                </div>
+                <div class="userdetails">
+                    <p id="user-followers">Followers : ${data.followers}</p>
+                    <p id="user-location">Location : ${data.location}</p>
+                    <p id="user-blog">Website : ${data.blog !== 'Unavailable' ? `<a href=${data.blog} target="_blank">${data.blog}</a>`: `${data.blog}`}</p>
+                    <p id="user-public-repositories">Public Repositories : ${data.public_repos}</p>
+                    <p id="user-bio">Description : ${data.bio}</p>
+            </div>`
 
             console.log(data.blog)
-            if (data.blog !== 'Unavailable') {userBlog.innerHTML = `Website : <a href=${data.blog} target="_blank">${data.blog}</a>`;
-            } else {userBlog.innerHTML = `Website : ${data.blog}`}
         })
 })
